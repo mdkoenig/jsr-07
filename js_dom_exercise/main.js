bb2fc6a22c6edda8ad74fecf89745cee30e7a211
Mark <mdkoenig@gmail.com>
2018-01-25 22:21:46 -0500

    Raindrops on roses

--- js_dom_exercise/main.js
+++ js_dom_exercise/main.js
@@ -9,16 +9,52 @@ Making a favorites list: DOM manipulation
 - Also, when a new item is added to the list, clear the input box.  
 
 */
+var things = [];
 
-function addToList(list, newThing) {
+document.getElementById("new-thing-button").onclick = getInfo;
 
+function getInfo()
+{
+	var thing = document.getElementById("new-thing").value;
+	//if (thing === "") {
+	//	alert("You must type in a value!")
+	//}
+	//else {
+		addToList(thing);
+		return false;
+	//}
 }
 
-window.onload = function() {
+function addToList(newThing) {
+	var li = document.createElement("li");
+	li.className = "fav-thing";
+	li.innerHTML = newThing;
+	document.getElementById("fav-list").appendChild(li);
+	document.getElementById("new-thing").value = "";
+	clean();
+}
+
+function clean () {
+	var child = 0;
+	var list = document.getElementById("fav-list");
+	for(i = 0; i < list.childNodes.length; i++)
+	{
+		if(list.childNodes[i].className === "fav-thing") {
+			child = i;
+			break;
+		}
+	}
+	document.getElementById("fav-list").removeChild(list.childNodes[child]);
+
+	//console.log(list.childNodes[0]);	
+}
+
+//window.onload = function() {
+	
   // when someone clicks the button...
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
-};
+//};
 
 /*
 
